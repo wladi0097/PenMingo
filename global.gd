778467aeda870676508654d60f9a2e2c9a4c9b6c
wladi <wladi0097@googlemail.com>
2021-11-22 17:30:42 +0100

    add zoo level

--- global.gd
+++ global.gd
@@ -4,4 +4,4 @@ var player: KinematicBody2D
 
 func _ready():
 	var master_sound = AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_mute(master_sound, true)
+#	AudioServer.set_bus_mute(master_sound, true)
